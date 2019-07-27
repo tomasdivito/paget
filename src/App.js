115865273021d8e24d5008d60fc559d6fe48f2bc
Tomas Di Vito <tomasdv2@gmail.com>
2019-07-27 16:38:13 -0300

    implemented wip page with only landing page

--- src/App.js
+++ src/App.js
@@ -1,32 +1,10 @@
-import React, { useState } from 'react';
+import React from 'react';
 
 import './App.scss';
-
-import Gallery from './components/gallery/gallery';
 
 const Images = require.context("./images");
 
 function App() {
-  let [currentGallery, setCurrentGallery] = useState(null);
-
-  let changeGallery = (newGallery) => {
-    document.querySelector('#gallery').scrollIntoView();
-
-    if (currentGallery === newGallery) {
-      setCurrentGallery(null);
-    }
-
-    // This is a **DIRTY HACK** in order to prevent a bug
-    // that can be reproduced in this way:
-    // Select `About` in the Landing.
-    // In the Gallery select the `Portfolio` button.
-    // Go back to Landing and if you try to select `About` it
-    // won't change since it's already selected. So we rewrite the
-    // state with `null` first and we introduced the real change under
-    // a timeout in order for the `null` state to take effect first.
-    setTimeout(() => setCurrentGallery(newGallery), 0);
-  };
-
   return (
     <div id="app">
       <section className="landing">
@@ -43,19 +21,7 @@ function App() {
             <img className="social-link" src={Images('./Twitter.svg')} alt="Twitter logo" />
           </a>
         </div>
-        <div className="page-links">
-          <a className="page-link" href="#about" onClick={() => changeGallery('about')}>
-            About Me
-          </a>
-          <a className="page-link" href="#portfolio" onClick={() => changeGallery('portfolio')}>
-            Portfolio
-          </a>
-          <a className="page-link" href="#contact" onClick={() => changeGallery('contact')}>
-            Contact
-          </a>
-        </div>
       </section>
-      <Gallery current={currentGallery} />
     </div>
   );
 }
