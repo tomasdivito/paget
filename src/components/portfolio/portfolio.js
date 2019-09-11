65750af8943eed5eb3954867eb1e3f9d4f46a1f8
Tomas Di Vito <tomasdv2@gmail.com>
2019-09-11 13:15:42 -0300

    working on various changes related to the portfolio showcase

--- src/components/portfolio/portfolio.js
+++ src/components/portfolio/portfolio.js
@@ -1,10 +1,11 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-import Project from './project';
+import ProjectButton from './project_button';
 import PROJECTS_JSON from '../../projects/project_list.json';
 import './project.scss';
 
 export default () => {
+  const [selected, setSelected] = useState({ code: null });
   const PROJECTS = PROJECTS_JSON.projects;
 
   return (
@@ -12,7 +13,12 @@ export default () => {
       <h1>Projects Showcase</h1>
       <div className="portfolio-gallery">
         {PROJECTS.map(project => (
-          <Project key={project.code} project={project} />
+          <ProjectButton
+            selected={project.code === selected.code}
+            evt={() => setSelected(project)}
+            key={project.code}
+            project={project}
+          />
         ))}
       </div>
     </div>
