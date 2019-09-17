c60d6c462b5dcfaf764306ca292990dcf2686a10
Tomas Di Vito <tomasdv2@gmail.com>
2019-09-17 12:34:54 -0300

    various changes to portfolio

--- src/components/about/about_me.js
+++ src/components/about/about_me.js
@@ -1,26 +1,11 @@
 import React from 'react';
+import { importMDX } from 'mdx.macro';
 
-import Skill from '../skill/skill';
 import './about_me.scss';
 
 const Avatar = require('../../images/toti-pixel.png');
 
-const skills = [
-  "Javascript",
-  "React",
-  "React Native",
-  "Redux",
-  "AngularJS",
-  "HTML",
-  "CSS",
-  "Jasmine",
-  "Karma",
-  "Rust",
-  "Ruby",
-  "C#",
-  "ASP.net",
-  "Webpack"
-];
+const AboutDocument = React.lazy(() => importMDX('./about_me.md'));
 
 export default () => (
   <div className="about-me-section">
@@ -30,14 +15,9 @@ export default () => (
         <img alt="toti-logo" className="logo" src={Avatar} />
       </div>
       <div className="about-me-summary">
-        <p>
-          I’m a Computer Engineering student at Universidad Nacional de La Matanza, I started working professionally as a
-          software developer in 2016 although I started learning programming when I was 14 years old. I worked for personal
-          projects and for clients as a freelancer. I have experience in the web using <b>Javascript</b>,
-          <b>React</b>, <b>AngularJS</b> and the tools surrounding them. I also have experience on backend technologies using <b>Ruby on Rails</b>,
-          <b>ASP.net</b> with <b>C#</b> and knowledge of other languages like <b>Java</b> and <b>C/C++</b>. I have interest in languages like <b>Rust</b> and
-          <b>elixir</b>, game development and graphics programming in general.
-        </p>
+        <React.Suspense fallback={<div>Loading...</div>}>
+          <AboutDocument />
+        </React.Suspense>
       </div>
     </div>
   </div>
